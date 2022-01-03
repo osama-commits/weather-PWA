09ec8762a9fccf08736ffc2e4680844d8e9795ae
osama pervez abdul rahman <osamahere313@gmail.com>
2022-01-03 16:49:25 +0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,57 @@
+import React, { useState } from "react";
+
+import fetchWeather from "./api/fetchWeather";
+
+import "./App.css";
+
+const App = () => {
+  const [query, setQuery] = useState("");
+
+  // to store the data in the weather variable
+  const [weather, setWeather] = useState({});
+
+  //   fetching the data based on query
+  const search = async (e) => {
+    if (e.key === "Enter") {
+      const data = await fetchWeather(query);
+
+      setWeather(data);
+      setQuery("");
+    }
+  };
+
+  return (
+    <div className="main-container">
+      <input
+        type="text"
+        className="search"
+        placeholder="Enter the city name..."
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+        onKeyPress={search}
+      />
+      {weather.main && (
+        <div className="city">
+          <h2 className="city-name">
+            <span>{weather.name}</span>
+            <sup>{weather.sys.country}</sup>
+          </h2>
+          <div className="city-temp">
+            {Math.round(weather.main.temp)}
+            <sup>&deg;C</sup>
+          </div>
+          <div className="info">
+            <img
+              className="city-icon"
+              src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
+              alt={weather.weather[0].description}
+            />
+            <p>{weather.weather[0].description}</p>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default App;
---
+++ src/api/fetchWeather.js
@@ -0,0 +1,20 @@
+import axios from "axios";
+
+import React from "react";
+
+const url = "https://api.openweathermap.org/data/2.5/weather";
+const api_key = "f33a484cf794d08d0148764789aaba32";
+
+const fetchWeather = async (query) => {
+  const { data } = await axios.get(url, {
+    params: {
+      q: query,
+      units: "metric",
+      APPID: api_key,
+    },
+  }); //axios is to perform a get request on the api
+
+  return data;
+};
+
+export default fetchWeather;
